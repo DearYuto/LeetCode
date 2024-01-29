6070aec5a4da9506dbe878a0876b14e9977c1e1d
유토 <154968122+DearYuto@users.noreply.github.com>
2024-01-29 15:55:52 +0900

    Time: 66 ms (10.97%), Space: 49 MB (5.81%) - LeetHub

---
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} start
+ * @return {number}
+ */
+var getMinDistance = function(nums, target, start) {
+    let result = Infinity;
+    
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] === target){
+            result = Math.min(result, Math.abs(i - start));
+        }
+    }
+     
+    return result;
+};
